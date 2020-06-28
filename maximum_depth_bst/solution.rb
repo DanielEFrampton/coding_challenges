b01a585740c3e6c68304cbf6a2fb02ef950f8ad9
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-06-28 13:35:51 -0600

    Create Ruby solution to maximum depth of BST challenge

---
+++ maximum_depth_bst/solution.rb
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+
+# Returns 1 + the greater of the max depth of the left branch or the right branch
+
+def max_depth(root)
+    return 0 if root == nil
+    1 + [max_depth(root.left), max_depth(root.right)].max
+end
